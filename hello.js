840ac7851aedc4d603f11d966d130a0b0a33e8a6
Lea Pehar <leapehar@gmail.com>
2021-10-15 17:14:25 +0000

    even exercise

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ const sayHello = function () {
 sayHello();
 */
 
-
+/*
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
@@ -14,3 +14,30 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+*/
+
+
+//console.log:
+
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+
+//return:
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
